7a26390a35490cf607ba00d1b24ea5ee82206411
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2016-08-03 20:26:58 +0200

    video 12: Mete los contactos en un array de objetos e itera sobre ellos para pintar la lista completa, también le da un valor key distinto a cada uno

--- src/Apps.js
+++ src/Apps.js
@@ -3,10 +3,23 @@ import {render} from 'react-dom';
 
 import ContactsList from './ContactsList'
 
-let contacts = {
+let contacts = [{
+	id: 1,
 	name: 'Scott',
 	phone: '555 555 5555'
-}
+},{
+	id: 2,
+	name: 'Courtney',
+	phone: '555 111 5555'
+},{
+	id: 3,
+	name: 'Tim',
+	phone: '555 333 5555'
+},{
+	id: 4,
+	name: 'Jeff',
+	phone: '555 555 1111'
+},]
 
 class App extends React.Component {
 	render() {
